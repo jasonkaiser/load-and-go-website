0d7c691630b6c4934a654ea9b05c19efe882d591
Jason Kaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-08-06 13:37:11 +0200

    Bug fixes

--- src/components/ui/inputform.tsx
+++ src/components/ui/inputform.tsx
@@ -9,7 +9,7 @@ const InputForm = ({ placeholder, type, width = "w-full", name, value, onChange,
     bg-black placeholder-gray-400 placeholder:text-[14px] text-gray-200 sequelFont !outline-none 
     border-2 border-transparent focus:border-primary focus:ring-4 focus:ring-primary/40 transition duration-300 
     max-md:w-full max-md:text-left
-    ${isDateOrTime ? "rounded-2xl max-md:rounded-lg p-3 mr-4 w-auto md:w-full md:p-4 md:pl-6" : "rounded-2xl w-full p-4 pl-6"}
+    ${isDateOrTime ? "rounded-2xl max-md:rounded-lg p-3 mr-4 place-self-center md:w-full md:p-4 md:pl-6" : "rounded-2xl w-full p-4 pl-6"}
   `;
 
   return (
